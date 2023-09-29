8eaf273d6978bf6b68b228ab061fe0664d3402e0
austintynes <austintynes10@gmail.com>
2023-09-28 23:01:44 -0500

    site now saves and loads the data of the first userInput box/ havent debugged the others

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,15 @@ let time = $(timeEl).text(today.format("h"));
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+//Saves
+document.addEventListener("DOMContentLoaded", function () {
+  let textInput = document.querySelector("textarea");
+  let savedTask = localStorage.getItem("savedTask");
+  if (savedTask) {
+    textInput.value = savedTask;
+  }
+});
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   let saveButton = document.querySelector(".saveBtn");
@@ -18,7 +27,16 @@ $(function () {
   saveButton.addEventListener("click", function (event) {
     let userInput = textInput.value;
     localStorage.setItem("savedTask", userInput);
+
     console.log("Saved:" + userInput);
+
+    document.addEventListener("DOMContentLoaded", function () {
+      let textInput = document.querySelector("textarea");
+      let savedTask = localStorage.getItem("savedTask");
+      if (savedTask) {
+        textInput.value = savedTask;
+      }
+    });
   });
 
   // use the id in the containing time-block as a key to save the user input in
@@ -38,12 +56,20 @@ $(function () {
       $(this).addClass("future");
     }
   });
+
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
+  document.addEventListener("DOMContentLoaded", function () {
+    let textInput = document.querySelector("textarea");
+    let savedTask = localStorage.getItem("savedTask");
+    if (savedTask) {
+      textInput.value = savedTask;
+    }
+  });
 
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
